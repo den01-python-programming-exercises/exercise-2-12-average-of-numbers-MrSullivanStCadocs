4342438fb44b25da3fffe648fe1ecbabb5ad4b69
MrSullivan <>
2021-04-20 22:15:50 +0000

    added average sum

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #write your code below this line
 
+    amountOfNumbers = 0
+    sumOfNumbers = 0
+
+    while True:
+      number = int(input("Give a number:"))
+
+      if (number == 0):
+        break
+      
+      if (number != 0):
+        amountOfNumbers = amountOfNumbers + 1
+        sumOfNumbers = sumOfNumbers + number
+    
+    print("Average of the numbers: " + str(sumOfNumbers/amountOfNumbers))
+
 if __name__ == '__main__':
     main()
